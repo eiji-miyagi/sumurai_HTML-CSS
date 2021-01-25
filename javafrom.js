eab05cfda6407e4e21b6eafac53df4aff2f04167
宮城　永次 <miyagieiji@miyagieijinoMacBook-Pro.local>
2021-01-25 18:07:52 +0900

    sekand commit

--- javafrom.js
+++ javafrom.js
@@ -1,4 +1,23 @@
 'use strct'
+// プランの関数の設定
+
+function Plan(time,Data){
+       
+    if(Data < 1) {
+        return time*20+2980
+    }if(Data >= 1 && Data< 4){
+        return time*20+3980
+        
+    }if(Data >= 4){
+        return time*20+4980
+        
+    }else{
+        return time*20+0
+    }
+ 
+}
+
+
 // ボタン情報の取得
 const btn = document.getElementById('btn');
 
@@ -15,3 +34,26 @@ btn.addEventListener('click',() => {
     document.body.appendChild(p)
 
 })
+const btn2 = document.getElementById('btn2');
+
+
+
+// ボタン２の設定
+btn2.addEventListener('click',() => {
+    const Talk = document.getElementById('Mytelephone').Talktaime.value;
+    const Datalog = document.getElementById('Mytelephone').Data.value;
+    
+    
+    const p2 = document.createElement('p');
+    　　　// P要素にテキストを入力
+    p2.textContent = 'au料金は'+ Plan(Talk,Datalog) + '円です'
+        // P要素を生成
+    document.body.appendChild(p2)
+
+
+    
+});
+
+
+
+
